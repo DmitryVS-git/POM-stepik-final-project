89987fe668e2d17eba3ed5ce1b8446e02757518f
Dmitry Voskoboynik <dmitry.voskoboynik@yandex.ru>
2023-05-22 13:07:55 +0300

    Add "is_element_present" method in base_page. Add new method in main_page and new test for it

--- test_main_page.py
+++ test_main_page.py
@@ -1,16 +1,19 @@
+import pytest
+
 from pages.main_page import MainPage
 
 
-link = "http://selenium1py.pythonanywhere.com/"
-
-
+@pytest.mark.skip
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
-    page = MainPage(browser, link) # Инициализируем PageObject, передаём в конструктор экземпляр драйвера и url
+    page = MainPage(browser, link)  # Инициализируем PageObject, передаём в конструктор экземпляр драйвера и url
     page.open()
 
     page.go_to_login_page()
 
 
-
-
+def test_guest_should_see_login_link(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link, timeout=2)
+    page.open()
+    page.should_be_login_link()
