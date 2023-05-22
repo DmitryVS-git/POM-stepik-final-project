dbbea06db18f0b73e0fe325ded3801d2503eb95d
Dmitry Voskoboynik <dmitry.voskoboynik@yandex.ru>
2023-05-22 13:53:05 +0300

    Add new files: locators, login page. Modify "Main page" file and "Main page" tests

--- test_main_page.py
+++ test_main_page.py
@@ -1,17 +1,22 @@
 import pytest
 
 from pages.main_page import MainPage
+from pages.login_page import LoginPage
+
+
+
+def test_guest_can_go_to_login_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link, timeout=2)  # Инициализируем PageObject, передаём в конструктор экземпляр драйвера и url
+    page.open()
+    page.go_to_login_page()
+
+    login_page_link = 'http://selenium1py.pythonanywhere.com/en-gb/accounts/login/'
+    login_page = LoginPage(browser, login_page_link, timeout=2)
+    login_page.should_be_login_page()
 
 
 @pytest.mark.skip
-def test_guest_can_go_to_login_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
-    page = MainPage(browser, link)  # Инициализируем PageObject, передаём в конструктор экземпляр драйвера и url
-    page.open()
-
-    page.go_to_login_page()
-
-
 def test_guest_should_see_login_link(browser):
     link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link, timeout=2)
