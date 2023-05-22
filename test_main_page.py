8a0eb0d1b7e035a73f432054dd98d8d890deb501
Dmitry Voskoboynik <dmitry.voskoboynik@yandex.ru>
2023-05-22 12:42:50 +0300

    Change realization "Main page" in test and in page file

--- test_main_page.py
+++ test_main_page.py
@@ -1,13 +1,16 @@
-from selenium.webdriver.common.by import By
+from pages.main_page import MainPage
+
 
 link = "http://selenium1py.pythonanywhere.com/"
 
-class MainPage:
-    def test_guest_can_go_to_login_page(browser):
-        browser.get(link)
-        go_to_login_page(browser)
+
+def test_guest_can_go_to_login_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link) # Инициализируем PageObject, передаём в конструктор экземпляр драйвера и url
+    page.open()
+
+    page.go_to_login_page()
 
 
-    def go_to_login_page(browser):
-        login_link = browser.find_element(By.CSS_SELECTOR, "#login_link")
-        login_link.click()
+
+
