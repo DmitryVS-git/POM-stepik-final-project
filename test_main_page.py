fc4095e567fdd2b01641d3314badee763a346b7c
Dmitry Voskoboynik <dmitry.voskoboynik@yandex.ru>
2023-05-23 11:59:55 +0300

    Change arguments in test_main_page

--- test_main_page.py
+++ test_main_page.py
@@ -7,16 +7,13 @@ from pages.login_page import LoginPage
 
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
-    page = MainPage(browser, link, timeout=2)  # Инициализируем PageObject, передаём в конструктор экземпляр драйвера и url
+    page = MainPage(browser, link, timeout=5)  # Инициализируем PageObject, передаём в конструктор экземпляр драйвера и url
     page.open()
     page.go_to_login_page()
-
-    login_page_link = 'http://selenium1py.pythonanywhere.com/en-gb/accounts/login/'
-    login_page = LoginPage(browser, login_page_link, timeout=2)
+    login_page = LoginPage(browser, browser.current_url, timeout=2)
     login_page.should_be_login_page()
 
 
-@pytest.mark.skip
 def test_guest_should_see_login_link(browser):
     link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link, timeout=2)
